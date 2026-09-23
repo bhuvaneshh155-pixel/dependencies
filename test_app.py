e1ad08cba8ac7efcb6f2a502eaf50a4421574995
bhuvaneshh155-pixel <bhuvaneshh155@gmail.com>
2026-09-23 17:08:13 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,21 +3,20 @@ from app import find_min, count_odds
 
 
 @pytest.mark.parametrize("numbers, expected", [
-    ([3, 1, 4, 1, 5], 1),         
-    ([-1, -5, 0, 2], -5),        
-    ([7], 7),                    
-    ([10, 20, 30], 99)           
-])
+    ([3, 1, 4, 2], 1),            
+    ([-1, -5, 0, 2], -5),          
+    ([7], 7),                      
+    ([10, 20, 30], 99)             
+]
 def test_find_min(numbers, expected):
     assert find_min(numbers) == expected
 
 
-
+# 3 valid test cases
 @pytest.mark.parametrize("numbers, expected", [
-    ([1, 2, 3, 4, 5], 3),        
-    ([2, 4, 6, 8], 0),           
-    ([1, 3, 5, 7], 4)            
+    ([1, 2, 3, 4, 5], 3),          
+    ([2, 4, 6, 8], 0),            
+    ([1, 3, 5, 7], 4)             
 ])
 def test_count_odds(numbers, expected):
     assert count_odds(numbers) == expected
-
